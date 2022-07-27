1958feec2e507a21c0dc7ce41041ed4d8f8fc5ae
Sebastiano <sebastiano.fazzino@swagyourlife.com>
2022-07-27 09:04:56 +0300

    navbar styles

--- personal-portfolio/frontend/src/components/NavBar/NavBar.jsx
+++ personal-portfolio/frontend/src/components/NavBar/NavBar.jsx
@@ -1,13 +1,23 @@
 import React from "react";
-
 import { images } from "../../constants";
+import './NavBar.scss';
+
+const menu = ['home', 'about', 'work', 'skills', 'contact'];
 
 const NavBar = () => {
     return (
-        <nav>
-            <div>
+        <nav className="app__navbar">
+            <div className="app__navbar-logo">
                 <img src={images.logo} alt='logo' />
-            </div>
+            </div>    
+            <ul className="app__navbar-links">
+                 { menu.map((item) => (
+                     <li className="app__flex p-text" key={`link-${item}`} >
+                         <div />
+                         <a href={`#${item}`}>{item}</a>
+                    </li>
+                )) }
+            </ul>
         </nav>
     );
 }
